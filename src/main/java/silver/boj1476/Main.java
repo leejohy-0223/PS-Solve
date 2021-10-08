6ef280bc8ebd4a21958921e910646bc327ca91c2
leejohy-0223 <leejohy@naver.com>
2021-10-08 18:25:58 +0900

    (2021/10/8) update PS

---
+++ src/main/java/silver/boj1476/Main.java
@@ -0,0 +1,21 @@
+package silver.boj1476;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+
+    }
+}
