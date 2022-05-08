a5d9ac01498ddd43f0ab25ccc9e0a469c250e97b
leejohy-0223 <leejohy@naver.com>
2022-05-08 22:32:24 +0900

    (2022/05/08) PS solve

---
+++ src/main/java/programmers/kakao/p4/SolutionRef.java
@@ -0,0 +1,11 @@
+package programmers.kakao.p4;
+
+public class SolutionRef {
+    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        int[] answer = new int[2];
+
+
+
+        return null;
+    }
+}
